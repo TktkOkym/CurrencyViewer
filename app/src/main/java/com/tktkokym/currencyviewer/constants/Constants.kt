78235ce1e09a9a93becca33232e48b49ead31e43
TktkOkym <taewonsuwon@gmail.com>
2019-09-20 09:59:53 +0900

    add extension TCs

--- app/src/main/java/com/tktkokym/currencyviewer/constants/Constants.kt
+++ app/src/main/java/com/tktkokym/currencyviewer/constants/Constants.kt
@@ -2,7 +2,7 @@ package com.tktkokym.currencyviewer.constants
 
 object Constants {
     const val THIRTY_MINUTES = 30 * 60 * 1000
-    const val DATE_FORMAT = "yyyy-mm-dd HH:MM"
+    const val DATE_FORMAT = "yyyy-MM-dd HH:mm"
     const val DEBOUNCE_DELAY = 350L
     const val WORKER_TAG = "CurrencyPeriodicWorker"
 }
--- app/src/test/java/com/tktkokym/currencyviewer/util/ExtensionsTest.kt
+++ app/src/test/java/com/tktkokym/currencyviewer/util/ExtensionsTest.kt
@@ -1,5 +1,6 @@
 package com.tktkokym.currencyviewer.util
 
+import com.tktkokym.currencyviewer.data.CurrencyItem
 import org.junit.Assert.*
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -37,5 +38,25 @@ class ExtensionsTest {
         assertTrue("00".isWrongDoubleFormat())
         assertTrue("1.0.".isWrongDoubleFormat())
         assertFalse("1.0".isWrongDoubleFormat())
+    }
+
+    @Test
+    fun testCurrencyMapToList() {
+        val expected = listOf(
+            CurrencyItem("USDUSD", 1.0),
+            CurrencyItem("USDJPY", 100.0),
+            CurrencyItem("USDKRW", 1000.0)
+        )
+
+        assertEquals(
+            expected,
+            mapOf("USDUSD" to 1.0, "USDJPY" to 100.0, "USDKRW" to 1000.0).convertToCurrencyList())
+    }
+
+    @Test
+    fun testFormatDateString() {
+        val unixTimeStamp = 1568854250L
+        val expected = "last updated: 2019-09-19 09:50"
+        assertEquals(expected, unixTimeStamp.formatDateString())
     }
 }
